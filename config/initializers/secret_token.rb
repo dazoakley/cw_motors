822f4fcc906a55f665e646cf7546da037b1efbcc
Daz Oakley <do2@sanger.ac.uk>
2011-03-19 08:51:04 +0000

    Initial jRuby, Rails 3, PostgreSQL skeleton - 'rails new cw_motors -d postgresql -m http://jruby.org/rails3.rb'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CwMotors::Application.config.secret_token = 'a8933163be615b46b41bd4d093d35d0e409e78a3669ca12892b7ed3b406653d46a2cea538da45da6a4fd6ebc9ccf2cdf7fd77d396a056e7276254571bf7daa7c'
