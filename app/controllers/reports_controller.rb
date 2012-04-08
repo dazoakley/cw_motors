f77014c41fd6dfc9c9097ad40f4d0b807b241973
Darren Oakley <daz.oakley@gmail.com>
2012-04-08 16:09:14 +0100

    Add an 'All Invoices' report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,8 +17,42 @@ class ReportsController < ApplicationController
         :email,
         :telephone,
         :mobile
-      ]
+      ],
+      :order => [:id]
     )
+
     send_data_csv('customers.csv', @report.to_csv) if request.format == :csv
+  end
+
+  def invoices
+    @report = Invoice.report_table(
+      :all,
+      :include => { :customer => { :methods => :short_code, :only => [] } },
+      :methods => [ :total_labour, :total_parts ],
+      :only => [
+        :id,
+        :date,
+        'customer.short_code',
+        :make_model,
+        :registration,
+        :total_labour,
+        :total_parts,
+        :environmental,
+        :subtotal,
+        :vat_rate,
+        :vat,
+        :mot,
+        :total,
+        :paid,
+        :date_paid
+      ],
+      :order => [:id]
+    )
+
+    @report.rename_columns({
+      'customer.short_code' => 'customer'
+    })
+
+    send_data_csv('invoices.csv', @report.to_csv) if request.format == :csv
   end
 end
