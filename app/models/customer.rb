85916045f797d7a916d49cb558a98f24c09e4811
Darren Oakley <daz.oakley@gmail.com>
2012-04-08 16:11:11 +0100

    Make the customer#short_code a bit more reliable...

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,15 +10,15 @@ class Customer < ActiveRecord::Base
   validates_uniqueness_of :company, :scope => [:first_name, :last_name]
 
   def short_code
-    str =  ""
+    str =  []
     str << "#{self.company}" unless self.company.blank?
 
     if !self.company.blank? and !( self.first_name.blank? or self.last_name.blank? )
-      str << " - "
+      str << "-"
     end
 
     str << "#{self.first_name} " unless self.first_name.blank?
     str << "#{self.last_name}" unless self.last_name.blank?
-    str
+    str.join(' ')
   end
 end
